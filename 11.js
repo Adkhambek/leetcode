2aec56575105c7d5dc566c7d74c8a2fa138cba07
Adham <muzaffarov.adham@gmail.com>
2021-11-15 18:26:13 +0500

    Valid Anagram

---
+++ 11.js
@@ -0,0 +1,21 @@
+// 242. Valid Anagram
+// https://leetcode.com/problems/valid-anagram/
+
+const isAnagram = (s, t) => {
+    const map = {};
+    for(let i = 0; i < s.length; i++) {
+        if(!map[s[i]]) map[s[i]] = 0;
+        map[s[i]]++
+    }
+    for(let i = 0; i < t.length; i++) {
+        if(map[t[i]]) {
+            map[t[i]]--
+            if(map[t[i]] === 0) {
+                delete map[t[i]]
+            }  
+        }
+    }
+    return Object.keys(map).length === 0
+};
+
+console.log(isAnagram("anagras", "nagaram"));
