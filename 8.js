aed69f43cd7e4d44789a29da573db331d915d1c0
Adham <muzaffarov.adham@gmail.com>
2021-11-15 08:14:23 +0500

    climb stairs

---
+++ 8.js
@@ -0,0 +1,36 @@
+// const climbStairs = (n) => {
+//     let a = 1;
+//     let b = 1;
+//     while (n > 0) {
+//         b += a
+//         a = b - a
+//         n--
+//     }
+//     return a
+// };
+
+// console.log(climbStairs(4));
+// console.log(climbStairs(5));
+// console.log(climbStairs(6));
+// console.log(climbStairs(7));
+
+// Fibonacci Sequence
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+// const fibonacci = n => {
+//     let a = 0
+//     let b = 1
+//     let temp
+//     while(n > 0) {
+//         temp = a
+//         a += b
+//         b = temp
+//         n--
+//     }
+//     console.log(temp);
+// }
+// console.log(fibonacci(1));
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(4));
+// console.log(fibonacci(6));
