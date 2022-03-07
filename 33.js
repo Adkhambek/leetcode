f5054b019e58b71422aa741a62e209be9ff59969
Adham <muzaffarov.adham@gmail.com>
2022-03-07 18:36:25 +0500

    1929. Concatenation of Array

---
+++ 33.js
@@ -0,0 +1,8 @@
+// 1929. Concatenation of Array
+// https://leetcode.com/problems/concatenation-of-array/
+
+const getConcatenation = function (nums) {
+    return nums.concat(nums);
+};
+
+console.log(getConcatenation([1, 2, 1]));
